c703424441110b9cac4b8581432a04c7a8fb1b10
Tanay Gupta <tanay.gupta13865@gmail.com>
2025-01-24 01:57:23 +0530

    card

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,6 +2,9 @@
 import { Card } from "@/app/components/ui/project-card";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
 
 export default function ProjectsPage() {
   // Array of projects with unique names, descriptions, and links
@@ -38,6 +41,55 @@ export default function ProjectsPage() {
 
   return (
     <div className="pt-20">
+      <CardContainer className="inter-var ">
+      <CardBody className="bg-slate-500 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border flex flex-col justify-center items-center">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-white dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem
+          translateZ="100"
+          // rotateX={}
+          // rotateZ={-10}
+          className="w-full mt-4 items-center justify-center flex"
+        >
+          <Image
+            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          {/* <CardItem
+            translateZ={20}
+            translateX={-40}
+            as="button"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem> */}
+          <CardItem
+            translateZ={20}
+            // translateX={40}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
       <motion.div
         className="container mx-auto py-16 px-4 text-center"
         initial={{ opacity: 0, y: 10 }}
